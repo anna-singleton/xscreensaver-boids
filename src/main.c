de6177e7cea791509fdda215e0a3cb6b55d9f74e
anna-singleton <annabeths111@gmail.com>
2023-03-12 16:32:40 +0000

    add standalone mode for testing

--- src/main.c
+++ src/main.c
@@ -1,15 +1,53 @@
-#include <X11/X.h>
-#include <stdlib.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include "vroot.h"
 #include "boid.h"
 
-int main() {
-    Display *dpy = XOpenDisplay(getenv("DISPLAY"));
+enum ProgramMode {
+    STANDALONE,
+    XSCREENSAVER,
+};
 
-    Window root = DefaultRootWindow(dpy);
+int main(int argc, char** argv) {
+    enum ProgramMode mode = XSCREENSAVER;
+
+    int opt;
+    while ((opt = getopt(argc,argv,"s")) != -1){
+        switch (opt) {
+        case 's':
+            mode = STANDALONE;
+            printf("Running in standalone mode\n");
+            break;
+        default:
+            fprintf(stderr, "Unknown option.");
+            return 1;
+        }
+    }
+
+    Display *dpy = XOpenDisplay(getenv("DISPLAY"));
+    Window root;
+
+    switch (mode) {
+    case STANDALONE:
+        root = XCreateWindow(dpy, DefaultRootWindow(dpy), 0, 0, 1600, 900, 0, CopyFromParent,
+                                   InputOutput, CopyFromParent, 0, NULL);
+        XTextProperty p;
+        char* name = "boids";
+        XStringListToTextProperty(&name, 1, &p);
+        XSetWMName(dpy, root, &p);
+        XClassHint* ch = XAllocClassHint();
+        ch->res_name = "boids";
+        ch->res_class = "floating";
+        XSetClassHint(dpy, root, ch);
+        XMapWindow(dpy, root);
+        break;
+    case XSCREENSAVER:
+        root = DefaultRootWindow(dpy);
+        break;
+    }
 
     GC g = XCreateGC(dpy, root, 0, NULL);
 
